0efa9f38cc5c166a59148fbcac5b0772127c726d
Irvin Emmanuel Trujillo Díaz <ie.trujillo@hotmail.com>
2020-06-29 23:17:35 -0500

    Web service para descarga de amigos

---
+++ WebApplication1/Controllers/AmigosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class AmigosController : ApiController
+    {
+
+        [HttpGet]
+        public DescargaAmigosResponse DescargaAmigos() {
+            var respuesta = new DescargaAmigosResponse();
+            try
+            {
+                using (var db = new ApplicationDbContext())
+                {
+                    var usuario = db.Users.Find(User.Identity.GetUserId());
+                    respuesta.ListaAmigos = new List<Amigo>(usuario.Amigos);
+                }
+            }
+            catch(Exception e) {
+                respuesta.Estatus = 500;
+                respuesta.Mensaje = $"Error en el servidor: {e.ToString()}";
+            }
+            return respuesta;
+        } 
+
+    }
+}
---
+++ WebApplication1/Models/ResponseModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ClassicResponse { 
+        public int Estatus { get; set; }
+        public string Mensaje { get; set; }
+
+        public ClassicResponse() {
+            this.Estatus = 200;
+            this.Mensaje = "Ok";
+        }
+    }
+
+    public class DescargaAmigosResponse : ClassicResponse { 
+        public List<Amigo> ListaAmigos { get; set; }
+
+        public DescargaAmigosResponse() : base() {
+            this.ListaAmigos = new List<Amigo>();
+        }
+    
+    }
+}
